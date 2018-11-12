43a85db56d5cb853b17e5db230baa814c0448c44
magic-onion <alexander.m.truong@gmail.com>
2018-11-12 16:42:11 -0500

    added ApiCommunicator Module

---
+++ app/models/concerns/api_communicator.rb
@@ -0,0 +1,15 @@
+module ApiCommunicator
+
+  BASE_URL =
+
+
+end
+
+#powers query - all powers
+#https://comicvine.gamespot.com/api/powers/?api_key=aeedf98edd4d5d6c5bbdd34f7713483c55611cfb
+
+#single character query
+#https://comicvine.gamespot.com/api/character/4005-1525/?api_key=aeedf98edd4d5d6c5bbdd34f7713483c55611cfb&format=json&field_list=name,powers
+
+#character search query - currently for darkwing duck
+#https://comicvine.gamespot.com/api/characters/?api_key=aeedf98edd4d5d6c5bbdd34f7713483c55611cfb&format=json&limit=5&filter=name:darkwing%20duck&field_list=aliases,deck,api_detail_url,id
