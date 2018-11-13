ecd82f6076a523c279fe72f929ee9ae008b2b85d
brewchetta <chett.tiller@gmail.com>
2018-11-13 15:54:47 -0500

    Started work on accidental master plan

--- app/models/heros_team.rb
+++ app/models/heros_team.rb
@@ -2,4 +2,18 @@ class HerosTeam < ApplicationRecord
   belongs_to :team
 
   belongs_to :hero
+
+  validate :unique_hero
+
+  private
+
+  # Makes sure heroes are unique to a team
+  def unique_hero
+    HerosTeam.each do |ht|
+      if team_id == ht.team.id && hero_id == ht.hero.id
+
+      end        
+    end
+  end
+
 end
