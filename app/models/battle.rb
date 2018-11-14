9458cd04bf6c111ad1cd291793f5ecc774af14e2
magic-onion <alexander.m.truong@gmail.com>
2018-11-14 11:06:43 -0500

    finished initial battle funcitonality in model

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -1,5 +1,7 @@
 class Battle < ApplicationRecord
 
+  attr_reader :score_hash
+
   def teams
     array = []
     array << Team.find_by(id: self.team_id1)
@@ -8,51 +10,57 @@ class Battle < ApplicationRecord
   end
 
   def team1
-    Team.find_by(id: self.team_id1)
+    @team1 = Team.find_by(id: self.team_id1)
   end
 
   def team2
-    Team.find_by(id: self.team_id2)
+    @team2 = Team.find_by(id: self.team_id2)
   end
 
   #currently only 1 player from team 1 is being fought
   def fight
+    # score_hash = {:team1 => 0, :team2 => 0}
     team1 = self.team1
     team2 = self.team2
     team1_heroes = team1.heros
     team2_heroes = team2.heros
-    team1_heroes.each do |hero1|
-      team2_heroes.each {|hero2| self.hero_fight(hero1, hero2)}
+    team1_heroes.zip(team2_heroes).each do |duel|
+      self.hero_fight(duel[0], duel[1])
+      byebug
     end
-    #collects loser of fight and winner of fight
-    #tallies each side's victories
-    #displays a winning team
-    #displays a message indicating both sides have kissed and made up.
-
+    win_condition = score_hash[@team1] <=> score_hash[@team2]
+    if win_condition == 1
+      puts "#{@team1.roster_name} is the winner"
+    else
+      puts "#{@team2.roster_name} is the winner"
+    end
   end
 
-  #unsure of outcome if power.score for both heroes is equal
-  #this is currently returning nil and putting nothing out to console
+  
+
   def hero_fight(hero1, hero2)
-    byebug
+    @score_hash = {@team1 => 0, @team2 => 0}
     score1 = hero1.powers.sample.score
     score2 = hero2.powers.sample.score
     boo = score1 <=> score2
-    puts case boo
+      case boo
       #the puts method above is only for testing
-    when 1 then "#{hero1.name} beat #{hero2.name}"
-    when -1 then "#{hero2.name} beat #{hero1.name}"
-    else puts "#{hero1.name} tied with #{hero2.name}"
+      when 1
+        puts "#{hero1.name} beat #{hero2.name}"
+        @score_hash[@team1] += 1
+      when -1
+        puts "#{hero2.name} beat #{hero1.name}"
+        @score_hash[@team2] += 1
+     else
+        puts "#{hero1.name} tied with #{hero2.name}"
     end
+    @score_hash
   end
-  # if boo
-  #   puts "h1"
-  # elsif boo != true
-  #   puts "h2"
-  # else
-  #   puts "tie"
-  # end
-
-  #hero1 power sample score vs hero2power score
 
 end
+
+
+#collects loser of fight and winner of fight
+#tallies each side's victories
+#displays a winning team
+#displays a message indicating both sides have kissed and made up.
