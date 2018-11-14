e91799c967bfc1a1c8241228d3bef120896621b5
magic-onion <alexander.m.truong@gmail.com>
2018-11-14 10:26:16 -0500

    battle stuff

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -15,6 +15,7 @@ class Battle < ApplicationRecord
     Team.find_by(id: self.team_id2)
   end
 
+  #currently only 1 player from team 1 is being fought
   def fight
     team1 = self.team1
     team2 = self.team2
@@ -23,19 +24,34 @@ class Battle < ApplicationRecord
     team1_heroes.each do |hero1|
       team2_heroes.each {|hero2| self.hero_fight(hero1, hero2)}
     end
+    #collects loser of fight and winner of fight
+    #tallies each side's victories
+    #displays a winning team
+    #displays a message indicating both sides have kissed and made up.
 
   end
 
   #unsure of outcome if power.score for both heroes is equal
+  #this is currently returning nil and putting nothing out to console
   def hero_fight(hero1, hero2)
+    byebug
     score1 = hero1.powers.sample.score
     score2 = hero2.powers.sample.score
     boo = score1 <=> score2
-      case boo
-      when true then "#{hero1.name} beat #{hero2.name}"
-      when false then "#{hero2.name} beat #{hero1.name}"
-      end
+    puts case boo
+      #the puts method above is only for testing
+    when 1 then "#{hero1.name} beat #{hero2.name}"
+    when -1 then "#{hero2.name} beat #{hero1.name}"
+    else puts "#{hero1.name} tied with #{hero2.name}"
+    end
   end
+  # if boo
+  #   puts "h1"
+  # elsif boo != true
+  #   puts "h2"
+  # else
+  #   puts "tie"
+  # end
 
   #hero1 power sample score vs hero2power score
 
