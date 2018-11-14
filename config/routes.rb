48cc967ef85ca99af3e6e8bf346674855b585cc5
brewchetta <chett.tiller@gmail.com>
2018-11-14 13:02:22 -0500

    Invalid authenticate reroutes and displays error properly, logout now works

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   get "/heros/search", to: "heros#search", as: "heros_search"
+
   get "/heros/create_from_api", to: "heros#add_hero_from_cv", as: "heros_search_create"
+
   patch "user/:user_id/teams/:id/add-hero", to: "teams#add_hero", as: "add_heros_team"
+
   get 'sessions/login', to: 'sessions#new', as: 'login'
+
   post 'sessions/login', to: 'sessions#create', as: 'sessions'
+
+  delete "sessions/login", to: "sessions#destroy"
 
   resources :battles
   resources :powers
