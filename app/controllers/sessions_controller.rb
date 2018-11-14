4eb19bb1ef5a03367f5547bb204fe6dedb7a81ab
brewchetta <chett.tiller@gmail.com>
2018-11-14 12:03:07 -0500

    Logout now appears

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(name: params[:name])
     if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect_to @user
     else
       redirect_to login_path
